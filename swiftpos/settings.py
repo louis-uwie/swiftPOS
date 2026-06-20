93a24d71f17daa76c0a89dbf623cf5514f5b9aae
louisuwie <louisbinwagiii@gmail.com>
2026-06-20 15:43:09 +0800

    Add gunicorn and Azure startup config

--- swiftpos/settings.py
+++ swiftpos/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'swiftpos-api-c5a4buguhfd7c3a3.southeastasia-01.azurewebsites.net',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
